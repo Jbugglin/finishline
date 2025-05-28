f40062a173d8d3272bbb15feceee0d2735a04f67
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-05-27 22:29:28 -0400

    Update actions.js
    I added functions to get the time in hh:mm am/pm format, as well as the date in short format. This info is now printing out to console, but need to figure out getting it to csv.

--- actions.js
+++ actions.js
@@ -3,7 +3,8 @@
  */
 function driverFunction() {
     getJobNumber();     //Gets the job number
-    getUser();
+    getUser();          //Gets operator initials
+    getStartDate();
     outputInfo();       //Prints our info out - will be exporting to CSV best case scenario
     //
 }
@@ -18,6 +19,26 @@ function getUser() {
     return user;
 }
 
+function getStartDate() {
+    let startDate = new Date().toLocaleDateString('en-US');
+    return startDate;
+}
+
+function getStartTime() {
+    //Need hours and minutes in hh:mm am/pm format
+    let startTime = new Date();
+    
+    let timeStamp = startTime.toLocaleString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+    return timeStamp;
+}
+
 function outputInfo() {
-    console.log(getJobNumber() + ' ' + getUser());
+    console.log("Date: " +getStartDate());
+    console.log("Time: " +getStartTime());
+    console.log("Job#: " +getJobNumber());
+    console.log("Op-initials: " +getUser());
+
 }
